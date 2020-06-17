05225c6d870157dd2e7e91123045f754171c8ffa
Chanchal <chaanchalagrawal@gmail.com>
2020-06-17 00:53:46 -0400

    alignment of publicationView page

--- src/components/PublicationView.js
+++ src/components/PublicationView.js
@@ -3,19 +3,19 @@ import { Button } from 'reactstrap';
 
 class PublicationView extends Component{
     render(){
-        return (
+        return ( 
             <div className = "PublicationView" >
-                <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-
-                    <h1 class="display-4 float-left mr-5"> Publication Title </h1>
-                    
-                    <Button href="#" variant="secondary" size="lg" disabled>  Previous </Button>{' '}
-                    <Button href="#" variant="secondary" size="lg" disabled> Next </Button>
+                <div class="container p-3 d-flex">
+                    <h1 class="display-4"> Publication Title </h1>
+                    <div class = "ml-auto" >
+                        <Button href="#" variant="secondary" size="lg" disabled>  Previous </Button>{' '}
+                        <Button href="#" variant="secondary" size="lg" disabled> Next </Button>
                     </div>
-                    </div>
+                </div>
+                <div class = "container">
+                    <img src={require("../images/bg2.jpg")} class="fluid" width="100%" height="250px" ></img>
                     <p>
-                                            Lorizzle ipsum dolor sit amizzle, consectetizzle adipiscing dawg. Nullam sapien velit, aliquet volutpizzle, suscipit izzle, gravida vel, gizzle. Pellentesque owned tortizzle. Sizzle eros. Dope fo shizzle mah nizzle fo rizzle, mah home g-dizzle dawg dapibus turpis black crackalackin. Uhuh ... yih! pellentesque nibh izzle turpizzle. Shizzle my nizzle crocodizzle izzle tortizzle. Break it down fo shizzle my nizzle rhoncus rizzle. In mofo break it down platea dictumst. Cool dapibus. Curabitizzle tellus urna, pretizzle fo shizzle, mattizzle the bizzle, eleifend vitae, nunc. Shizzlin dizzle suscipizzle. Rizzle rizzle velit you son of a bizzle brizzle.
+                        Lorizzle ipsum dolor sit amizzle, consectetizzle adipiscing dawg. Nullam sapien velit, aliquet volutpizzle, suscipit izzle, gravida vel, gizzle. Pellentesque owned tortizzle. Sizzle eros. Dope fo shizzle mah nizzle fo rizzle, mah home g-dizzle dawg dapibus turpis black crackalackin. Uhuh ... yih! pellentesque nibh izzle turpizzle. Shizzle my nizzle crocodizzle izzle tortizzle. Break it down fo shizzle my nizzle rhoncus rizzle. In mofo break it down platea dictumst. Cool dapibus. Curabitizzle tellus urna, pretizzle fo shizzle, mattizzle the bizzle, eleifend vitae, nunc. Shizzlin dizzle suscipizzle. Rizzle rizzle velit you son of a bizzle brizzle.
 
                         Etizzle a magna sed augue get down get down for sure. Fo shizzle izzle gangsta. Vivamizzle maurizzle dolizzle, viverra vitae, facilisizzle izzle, ghetto, erizzle. Vestibulizzle ante ipsum stuff izzle shiznit orci luctizzle izzle ultrices posuere gangster Its fo rizzle; Dizzle dolizzle. Integer faucibus. Maecenas nizzle blandit quizzle. Fizzle rutrizzle aliquet orci. Sed facilisizzle. Da bomb sizzle nulla, venenatis fizzle, scelerisque shiznit, blandit ac, magna.
 
@@ -27,10 +27,11 @@ class PublicationView extends Component{
 
                         Morbi interdum. Yippiyo potenti. Maecenizzle nisl. Dizzle funky fresh, owned quizzle, ullamcorpizzle ut, scelerisque crazy, leo. I'm in the shizzle that's the shizzle neque. Dizzle felis. Morbi nonummy, nisl bling bling fringilla cursus, libero mi varius felizzle, sed things da bomb the bizzle augue. Stuff yo mamma nibh vel we gonna chung. Fusce bibendizzle black nec libero. Fo shizzle my nizzle own yo', metizzle vizzle varizzle pimpin', pizzle erizzle pharetra bizzle, eu luctus risus sheezy sizzle mammasay mammasa mamma oo sa.
 
-
                     </p>
-                    
                 </div>
+            </div>
+                    
+                    
             
             
         );
